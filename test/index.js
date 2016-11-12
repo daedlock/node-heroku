ac738abb843ae96d2bd35fe1dfb877411b429fe0
Hossam Saraya <hossam.saraya@gmail.com>
2016-11-12 17:06:10 +0200

    Testing

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+
+describe('True Test', function(){
+
+  it('should always succeed', function(){
+    assert.strictEqual(true, true, 'these booleans are strictly equal');
+  })
+});
