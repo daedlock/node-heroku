73d0f9897631d33c63180b9d0388e3846c7b0bf9
Hossam Saraya <hossam.saraya@gmail.com>
2016-11-12 16:51:50 +0200

    Es6

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,16 @@ var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
 
-
-function getGithubUsers(handle) {
-  return fetch('https://api.github.com/users/' + handle)
-  .then(function(res) {
-    return res.json();
-  });
-}
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.json({
     msg: 'Hello World'
-  })
+  });
 });
 
 router.get('/users', function(req,res){
-  getGithubUsers(req.param('handle')).then(function(users) {
-    res.json(users);
-  });
+  const getGithubUsers = (handle) => fetch('https://api.github.com/users/' + handle)
+  getGithubUsers(req.param('handle')).then((users) => res.json(users));
 });
 
 module.exports = router;
