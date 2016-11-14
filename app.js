50a825899cb053b9ebc97b8a4ebace7f8a89be2c
Hossam Saraya <hossam.saraya@gmail.com>
2016-11-14 03:36:07 +0200

    Views

--- app.js
+++ app.js
@@ -16,20 +16,22 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,33 @@ var router = express.Router();
 var fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.json({
     msg: 'Hello World'
   });
 });
 
-router.get('/users', function(req,res){
+router.get('/users', function (req, res) {
   const getGithubUsers = (handle) => fetch('https://api.github.com/users/' + handle)
-  .then((res) => res.json());
+    .then((res) => res.json());
 
   getGithubUsers(req.param('handle')).then((users) => res.json(users));
 });
 
-
-router.get('/repo', function(req,res){
+router.get('/repo', function (req, res) {
   const getGithubRepo = (repo) => fetch('https://api.github.com/repos/' + repo)
-  .then((res) => res.json());
+    .then((res) => res.json());
 
   console.log(req.param('repo'));
 
   getGithubRepo(req.param('repo')).then((repo) => res.json(repo));
 });
 
-router.get('/time2', function(req,res){
-  res.json({time: new Date().getTime()});
+router.get('/time2', function (req, res) {
+  res.json({ time: new Date().getTime() });
 })
+
+router.get('/view', function (req, res) {
+  res.render('index');
+});
 module.exports = router;
